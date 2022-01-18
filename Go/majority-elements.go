d2a377cbbe3a3ca1d83e765d1c2cc056b8d48472
Bilguun <bilguunbek1108@gmail.com>
2022-01-18 23:43:10 +0800

    Added Majorit Element

--- Go/majority-elements.go
+++ Go/majority-elements.go
@@ -1,11 +1,14 @@
 package Go
 
 func majorityElement(nums []int) int {
+	//Counter and Chaning Majority Element Variables
 	var majority_element, counter int = nums[0], 1
+	//Looping through Slices to get Element
 	for idx := 1; idx < len(nums); idx++ {
 		if counter == 0 {
 			majority_element = nums[idx]
 		}
+		//Checking if each element is equal to current majority Element
 		if nums[idx] == majority_element {
 			counter++
 		} else {
