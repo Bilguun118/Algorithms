ac6b58b0d088a1a7146abfcea686b0877810ffcb
Bilguun <bilguunbek1108@gmail.com>
2022-01-30 00:25:49 +0800

    Added Rotate Array Tried to Fix String to Atoi

---
+++ Go/Rotate-Array.go
@@ -0,0 +1,20 @@
+package Go
+
+func rotate(nums []int, k int) {
+	//Getting How many Reverse Times
+	k = k % len(nums)
+	reverse(nums, 0, len(nums)-1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, len(nums)-1)
+}
+
+func reverse(nums []int, start, end int) {
+	// From Start to End loop
+	for start < end {
+		temp := nums[start]
+		nums[start] = nums[end]
+		nums[end] = temp
+		start++
+		end--
+	}
+}
