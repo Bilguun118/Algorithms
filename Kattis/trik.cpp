3f771c14d88705db5552899e79820d3af09154bd
bilguun <bilguunbek1108@gmail.com>
2023-07-26 11:28:04 +0800

    Added trik

--- Kattis/trik.cpp
+++ Kattis/trik.cpp
@@ -6,20 +6,41 @@
 using namespace std;
 
 int determine_trik(char cup, int ball) {
-    if(ball == 1) {
-
+    if(cup == 'A') {
+        if(ball == 1) {
+            ball = 2;
+        } else if (ball == 2) {
+            ball = 1;
+        } else {
+            ball = 3;
+        }
+    } else if (cup == 'B') {
+        if (ball == 1) {
+            ball = 1;
+        } else if (ball == 2) {
+            ball = 3;
+        } else {
+            ball = 2;
+        }
+    } else {
+        if (ball == 1) {
+            ball = 3;
+        } else if (ball == 2) {
+            ball = 2;
+        } else {
+            ball = 1;
+        }
     }
     return ball;
 }
 
 void trik(string cups) {
     int n = cups.size();
-    int det[3] = {1,0,0};
     int ball = 1;
     for(int i = 0; i < n; i++) {
         ball = determine_trik(cups[i], ball);
-        continue;
     }
+    cout << ball;
     return;
 }
 
