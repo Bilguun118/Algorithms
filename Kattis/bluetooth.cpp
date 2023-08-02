d4b56eb42d28a0c8638e6ad68b20bc063cdb8739
bilguun <bilguunbek1108@gmail.com>
2023-08-02 12:24:08 +0800

    Added new problem

--- Kattis/bluetooth.cpp
+++ Kattis/bluetooth.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <cstdlib>
-#include <unordered_map>
 #include <string>
 
 using namespace std;
@@ -10,9 +8,8 @@ int main() {
     cin >> TC;
     bool rotted_l, rotted_r;
     rotted_l = rotted_r = false;
-    // cout << rotted_l << " " << rotted_r << "\n";
-    int ans_right, ans_left;
-    ans_right = ans_left = 8;
+    int ans_right_lower, ans_left_lower, ans_right_upper, ans_left_upper;
+    ans_right_lower = ans_left_lower = ans_right_upper = ans_left_upper = 8;
     while(TC--){
         string teeth;
         char state;
@@ -27,19 +24,26 @@ int main() {
         }
         if (state == 'm') {
             // we should not count this teeth
-            if(teeth[0] == '-' || teeth[0] == '+') {
-                ans_left--;
+            if(teeth[0] == '-') {
+                ans_left_lower--;
+            } else if (teeth[0] == '+') {
+                ans_left_upper--;
+            } else if (teeth.back() == '-') {
+                ans_right_lower--;
             } else {
-                ans_right--;
+                ans_right_upper--;
             }
         }
     }
-    if(ans_right > 0 && !rotted_r) {
+    int left_tooth = ans_left_lower+ans_left_upper;
+    int right_tooth = ans_right_lower+ans_right_upper;
+
+    if(right_tooth > 0 && (ans_right_lower > 0 && ans_right_upper > 0) && !rotted_r) {
         cout << 1;
-    } else if (ans_left > 0 && !rotted_r) {
+    } else if (left_tooth > 0 && (ans_left_lower > 0 && ans_left_upper > 0) && !rotted_l) {
         cout << 0;
     } else {
         cout << 2;
     }
-    return 0; 
+    return 0;
 }
