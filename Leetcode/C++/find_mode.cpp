cdee7ff2441d927748ee5f767bab3a2cb243821e
bilguun <bilguunbek1108@gmail.com>
2023-11-20 21:11:52 +0800

    added find mode in binary tree

---
+++ Leetcode/C++/find_mode.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+};
+
+
+
+void findMode(TreeNode* current, vector<int>& ans, int& currentVal, int& freq, int& maxFreq) {
+    if(!current) return;
+    findMode(current->left, ans, currentVal, freq, maxFreq);
+    if(currentVal == current->val) {
+        freq++;
+    } else {
+        currentVal = current->val;
+        freq = 1;
+    }
+
+    if(freq > maxFreq) {
+        maxFreq = freq;
+        ans = {current->val};
+    } else if (freq == maxFreq) {
+        ans.push_back(current->val);
+    }
+    findMode(current->right, ans, currentVal, freq, maxFreq);
+}
+
+int main() {
+    TreeNode b;
+    b.val = 2;
+    b.left = NULL;
+    b.right = NULL;
+    TreeNode c;
+    c.val = 2;
+    c.left = &b;
+    c.right = NULL;
+    TreeNode a;
+    a.val = 1;
+    a.left = &c;
+    a.right = NULL;
+    vector<int>ans;
+    int currentVal = 0;
+    int freq = INT_MIN;
+    int maxFreq = 0;
+    findMode(&a, ans, currentVal, freq, maxFreq);
+    return 0;
+}
