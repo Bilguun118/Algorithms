2da51bb0b08664bcb2ac446ee98bf935b0f6d32a
bilguun <bilguunbek1108@gmail.com>
2023-12-20 11:43:35 +0800

    added jump game

--- Leetcode/C++/jump_game.cpp
+++ Leetcode/C++/jump_game.cpp
@@ -3,18 +3,33 @@
 
 using namespace std;
 
-bool canJump(vector<int>& nums) {
-    bool canjump = false;
-    for(int i = 0; i < nums.size();) {
-        i += nums[i];
-        cout << "THE INITIAL POSITION IS: " << i << "\n";
-    } 
-    return canjump;
+bool canJumpRc(int current_pos, vector<int>& nums) {
+    if(current_pos == nums.size()-1) return true;
+    if(nums[current_pos] == 0) return false;
+    int reach_point = current_pos + nums[current_pos];
+    for(int i = current_pos+1; i <= reach_point; i++) {
+        if(i < nums.size() && canJumpRc(i, nums)) return true;
+    }
+    return false;
+}
+
+bool canJumpTopToBottom(int current_pos, vector<int>& nums, vector<int>& dp) {
+    if(current_pos == nums.size()-1) return true;
+    if(nums[current_pos] == 0) return false;
+
+    if(dp[current_pos] != -1) return dp[current_pos];
+    int reach_point = current_pos + nums[current_pos];
+    for(int i = current_pos+1; i <= reach_point; i++) {
+        if(i < nums.size() && canJumpRc(i, nums)) return dp[current_pos] = true;
+    }
+    return dp[current_pos] = false;
 }
 
 int main() {
     vector<int>nums = {2,3,1,1,4};
     bool ans;
-    ans = canJump(nums);
+    vector<int>dp(nums.size(), -1);
+    ans = canJumpTopToBottom(0, nums, dp);
+    cout << ans;
     return 0;
 }
