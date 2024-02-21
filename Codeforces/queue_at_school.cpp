7e9921cc1735d87f5ce3dba2f4105e6f8ebd05c4
bilguun <bilguunbek1108@gmail.com>
2024-02-21 17:02:25 +0800

    added queue at school

--- Codeforces/queue_at_school.cpp
+++ Codeforces/queue_at_school.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main() {
+    int n, t;
+    string schoolQ;
+    cin >> n >> t;
+    cin >> schoolQ;
+    int count = t;
+    while(t--){
+        for(int j = 0; j < n; j++) {
+            if(schoolQ[j] == 'B' && schoolQ[j+1] == 'G' && j+1 < n) {
+                char temp = schoolQ[j+1];
+                schoolQ[j+1] = schoolQ[j];
+                schoolQ[j] = temp;
+                j = j+1;
+            };
+        }
+    }
+    cout << schoolQ;
     return 0;
 }
